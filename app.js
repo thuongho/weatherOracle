04d28d130117f4f5f9b56e091300edbc36544b27
Thuong Ho <thuong.t.ho@gmail.com>
2015-09-14 22:41:18 -0700

    add requirements, main and forecast pages

--- app.js
+++ app.js
@@ -1,5 +1,37 @@
-// Load Zurb
-$(document).foundation();
+(function () {
+  // Load Zurb
+  $(document).foundation();
 
-// MODULE
-var weatherApp = angular.module('weatherOracle', []);
+  // MODULE
+  var weatherApp = angular.module('weatherOracle', ['ngResource', 'ngRoute']);
+
+  // ROUTING
+  weatherApp.config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'pages/main.html',
+        controller: 'mainController'
+      })
+      .when('/forecast', {
+        templateUrl: 'pages/forecast.html',
+        controller: 'forecastController'  
+      })
+      .when('/requirements', {
+        templateUrl: 'pages/requirements.html',
+        controller: 'requirementsController'
+      })
+  });
+
+  // CONTROLLERS
+  weatherApp.controller('mainController', ['$scope', '$log', function ($scope, $log){
+    
+  }]);
+
+  weatherApp.controller('forecastController', ['$scope', function ($scope){
+    
+  }]);
+
+  weatherApp.controller('requirementsController', ['$scope', function ($scope){
+
+  }]);
+}());
