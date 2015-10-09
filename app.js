b1d9dacc017126d0f2e47ac718012d84c2a54ca2
Thuong Ho <thuong.t.ho@gmail.com>
2015-10-09 00:33:05 -0700

    add conversions for temp and date

--- app.js
+++ app.js
@@ -46,7 +46,21 @@
     $scope.weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast/daily", { callback: "JSON_CALLBACK"}, { get: { method: "JSONP" }});
 
     // assign the data
-    $scope.weatherResult = $scope.weatherAPI.get({ q: $scope.city, cnt: 2 });
+    // params in the api url
+    $scope.weatherResult = $scope.weatherAPI.get({ q: $scope.cityName, cnt: 2 });
+
+    // console.log($scope.weatherResult);
+
+    // function to convert K to F
+    $scope.convertToFahrenheit = function(degK) {
+      return Math.round((1.8 * (degK -273)) + 32);
+    };
+
+    // function to convert time to human readable time
+    $scope.convertToDate = function(dt) {
+      // multiply 1000 to convert milliseconds to second
+      return new Date(dt * 1000);
+    };
     
   }]);
 
